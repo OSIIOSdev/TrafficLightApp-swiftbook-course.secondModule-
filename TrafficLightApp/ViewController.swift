da46ce8ff75f21df579372b7f97b545e04a45a9d
OSIIOSdev <OSIIOS.dev@gmail.com>
2023-07-18 21:44:39 -0700

    Initial Commit

---
+++ TrafficLightApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLightApp
+//
+//  Created by OSIIOSdev on 18.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
