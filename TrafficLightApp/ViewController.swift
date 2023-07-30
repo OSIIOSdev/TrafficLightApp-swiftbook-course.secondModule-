53db977fccf04f4f5b204b2a57919ab6e4a63d0c
OSIIOSdev <OSIIOS.dev@gmail.com>
2023-07-30 00:01:07 -0700

    1. App action logic configured

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -8,12 +8,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    enum Lights {
+        case red
+        case yellow
+        case green
+    }
+    
+    // MARK: - @IBOutlets
+    @IBOutlet var RedLightView: UIView!
+    @IBOutlet var YellowLightView: UIView!
+    @IBOutlet var GreenLightView: UIView!
+    
+    @IBOutlet var ButtonLightChanged: UIButton!
+    
+    // MARK: - Properties
+    private var currentLight = Lights.red
+    
+    private var lightOn: CGFloat = 1
+    private var lighOff: CGFloat = 0.3
+    
+    // MARK: - AppLifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setButtonConfig()
+        
+        setLightConfig(for: RedLightView, color: .systemRed)
+        setLightConfig(for: YellowLightView, color: .systemYellow)
+        setLightConfig(for: GreenLightView, color: .systemGreen)
     }
 
-
+    // MARK: - SettingsMethods
+    private func setButtonConfig() {
+        ButtonLightChanged.layer.cornerRadius = 15
+        ButtonLightChanged.setTitle("START", for: .normal)
+        ButtonLightChanged.tintColor = UIColor.white
+        ButtonLightChanged.backgroundColor = UIColor.black
+    }
+    
+    private func setLightConfig(for light: UIView, color: UIColor) {
+        light.layer.cornerRadius = light.frame.width / 2
+        light.backgroundColor = color
+        light.alpha = lighOff
+    }
+    
+    // MARK: - @IBActionMethods
+    @IBAction func getButtonPressedAction(_ sender: UIButton) {
+        sender.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            GreenLightView.alpha = lighOff
+            RedLightView.alpha = lightOn
+            currentLight = .yellow
+        case .yellow:
+            RedLightView.alpha = lighOff
+            YellowLightView.alpha = lightOn
+            currentLight = .green
+        case .green:
+            YellowLightView.alpha = lighOff
+            GreenLightView.alpha = lightOn
+            currentLight = .red
+        }
+    }
+    
+    
 }
 
